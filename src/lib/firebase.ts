f0dcbc3d9abe5a7a7bbd982747119ba50b676bf6
Upendra K Chaudhary <ukcbrh@gmail.com>
2025-08-23 19:05:27 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "courtbell",
+  "appId": "1:534647613176:web:94a5adf181e4d2182c3d0d",
+  "storageBucket": "courtbell.firebasestorage.app",
+  "apiKey": "AIzaSyA_2tzAd3PhEJH3lBZwXQ0ku-rxWbSPel4",
+  "authDomain": "courtbell.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "534647613176"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
